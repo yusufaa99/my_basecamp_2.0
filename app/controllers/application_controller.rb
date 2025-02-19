6ba6dd8f3ce958de47e959904a07aa4e12f40137
emmanu_mat <mattemmsmartprince@gmail.com>
2025-02-19 04:45:49 +0100

    Fixed some error & styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_project
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   private
 
