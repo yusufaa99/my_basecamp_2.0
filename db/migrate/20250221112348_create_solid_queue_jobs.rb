72a463803947efec017e1ba5b9a03feae3f7d239
Langtock <70757120+Langtok@users.noreply.github.com>
2025-02-21 15:01:34 +0100

    Fixed SolidQueueJobs migration

--- db/migrate/20250221112348_create_solid_queue_jobs.rb
+++ db/migrate/20250221112348_create_solid_queue_jobs.rb
@@ -1,10 +1,21 @@
 class CreateSolidQueueJobs < ActiveRecord::Migration[8.0]
   def change
     create_table :solid_queue_jobs do |t|
-      t.string :job_type
+      t.string :queue_name, null: false
+      t.string :class_name, null: false
       t.text :arguments
-      t.string :status, default: "pending"
-      t.timestamps
+      t.integer :priority, default: 0, null: false
+      t.string :active_job_id
+      t.datetime :scheduled_at
+      t.datetime :finished_at
+      t.string :concurrency_key
+      t.timestamps null: false
     end
+
+    add_index :solid_queue_jobs, :active_job_id, name: "index_solid_queue_jobs_on_active_job_id"
+    add_index :solid_queue_jobs, :class_name, name: "index_solid_queue_jobs_on_class_name"
+    add_index :solid_queue_jobs, :finished_at, name: "index_solid_queue_jobs_on_finished_at"
+    add_index :solid_queue_jobs, [:queue_name, :finished_at], name: "index_solid_queue_jobs_for_filtering"
+    add_index :solid_queue_jobs, [:scheduled_at, :finished_at], name: "index_solid_queue_jobs_for_alerting"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_14_175108) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_112348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -135,6 +135,24 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_14_175108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["creator_id"], name: "index_projects_on_creator_id"
+  end
+
+  create_table "solid_queue_jobs", force: :cascade do |t|
+    t.string "queue_name", null: false
+    t.string "class_name", null: false
+    t.text "arguments"
+    t.integer "priority", default: 0, null: false
+    t.string "active_job_id"
+    t.datetime "scheduled_at"
+    t.datetime "finished_at"
+    t.string "concurrency_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["active_job_id"], name: "index_solid_queue_jobs_on_active_job_id"
+    t.index ["class_name"], name: "index_solid_queue_jobs_on_class_name"
+    t.index ["finished_at"], name: "index_solid_queue_jobs_on_finished_at"
+    t.index ["queue_name", "finished_at"], name: "index_solid_queue_jobs_for_filtering"
+    t.index ["scheduled_at", "finished_at"], name: "index_solid_queue_jobs_for_alerting"
   end
 
   create_table "tasks", force: :cascade do |t|
