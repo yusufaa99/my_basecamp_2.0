e0dae3d22cf0521751721205b3dd41d5fb734414
Software Development9 <Software Development9@outsource-kd.ng>
2025-05-22 12:16:07 +0100

    working on edit profile page TO ADD PROFILE IMAGE TO CONTROLLER AND ROUTE

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,22 +1,45 @@
+# class ProfilesController < ApplicationController
+#   before_action :authenticate_user!
+
+#   def edit
+#     @user = current_user
+#   end
+
+#   def update
+#     @user = current_user
+#     if @user.update(profile_params)
+#       redirect_to root_path, notice: "Profile updated successfully."
+#     else
+#       render :edit, status: :unprocessable_entity
+#     end
+#   end
+
+#   private
+
+#   def profile_params
+#     params.require(:user).permit(:name, :bio)
+#   end
+# end
+
+# app/controllers/profiles_controller.rb
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!  # If using Devise or authentication
 
   def edit
     @user = current_user
   end
 
   def update
-    @user = current_user
-    if @user.update(profile_params)
-      redirect_to root_path, notice: "Profile updated successfully."
+    if current_user.update(profile_params)
+      redirect_to edit_profile_path, notice: "Profile updated successfully."
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
   private
 
   def profile_params
-    params.require(:user).permit(:name, :bio)
+    params.require(:user).permit(:name, :bio, :avatar)
   end
 end
